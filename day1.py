6b2c53f0897352c63320cf78ea91ed77f1439b80
Sarah Johnson <sozzifer@gmail.com>
2022-12-02 17:19:30 +0000

    Day 1

---
+++ day1.py
@@ -0,0 +1,14 @@
+with open("day1.txt") as f:
+    data = f.read()
+
+elves = data.split("\n\n")
+calories = [elf.split("\n") for elf in elves]
+
+day1_dict = dict()
+key = 1
+
+for calorie in calories:
+    day1_dict["elf" + str(key)] = sum([(int(cal)) for cal in calorie])
+    key += 1
+
+print(max(day1_dict.values()))
